e88839c435b08bd6d88180138cb5d7de221a91f0
MinervaLong <vanesa.perea.cuevas@gmail.com>
2021-05-24 09:30:39 +0200

    Exercise 3 - Level 3 with errors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
- import React, {useState} from 'react'
+ import React, {useState, useEffect} from 'react'
  import Header from './components/Header'
  import Joke from './components/Joke'
  import Btn from './components/Btn'
+ import Weather from './components/Weather'
  import Container from 'react-bootstrap/Container'
  import Row from 'react-bootstrap/Row'
  import Col from 'react-bootstrap/Col'
  import styled from 'styled-components';
+ import axios from 'axios' 
  
  const Main = styled.main`
     background: #2980B9; 
     background: linear-gradient(to bottom, #FFFFFF, #6DD5FA, #2980B9);
-    padding-top:5rem;
+    padding-top:3rem;
     height: 100vh;
  `
+ 
+ const API_KEY = process.env.REACT_APP_API_KEY;
+ const weatherUrl ='https://api.openweathermap.org/data/2.5/weather?q=Barcelona,ES&APPID=API_KEY';
 
  const fetchUrlJokes = 'https://geek-jokes.sameerkumar.website/api'
  
  const App = () => {
+     //Call to Weather API with Axios
+     const [weather, setWeather] = useState('');
+
+    useEffect(async () => {        
+        const result = await axios.get(weatherUrl) 
+        const {description} = result.data.weather[0] 
+        setWeather(description)
+      });
+    
+    //Call to Joke API
     const [joke, setJoke] = useState(null)
 
     const getJoke = () => fetch(fetchUrlJokes)
@@ -29,6 +44,7 @@
 
     return (
         <Main>
+            <Weather weather={weather}/>
         <Container className='p-5 flex justify-content-center shadow-lg p-3 mb-5 bg-white rounded'>
             <Row className="justify-content-md-center position-relative">
                 <Col xs lg='1' className='p-0'></Col>
@@ -45,8 +61,7 @@
                     
                 </Col>
                 <Col xs lg='1' className='p-0'></Col>
-            </Row>
-                      
+            </Row>                      
         </Container>
         </Main>
     );
---
+++ src/components/Weather.js
@@ -0,0 +1,7 @@
+const Weather = (props) => {
+    return(
+        <h4>Barcelona's forecast today: {props.weather}</h4>
+    )
+}
+
+export default Weather;
